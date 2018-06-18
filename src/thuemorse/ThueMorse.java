56cde0614bc68500fb2c591359e59684ffdeba58
Jos <josvanuden@gmail.com>
2018-06-18 07:45:38 +0200

    Tabs to space

--- src/thuemorse/ThueMorse.java
+++ src/thuemorse/ThueMorse.java
@@ -2,18 +2,18 @@ package thuemorse;
 
 public class ThueMorse {
 
-	public static void main(String[] args) {
-		sequence(6);
-	}
+    public static void main(String[] args) {
+        sequence(6);
+    }
 
-	public static void sequence(int steps) {
-		StringBuilder sb1 = new StringBuilder("0");
-		StringBuilder sb2 = new StringBuilder("1");
-		for (int i = 0; i < steps; i++) {
-			String tmp = sb1.toString();
-			sb1.append(sb2);
-			sb2.append(tmp);
-		}
-		System.out.println(sb1);
-	}
+    public static void sequence(int steps) {
+        StringBuilder sb1 = new StringBuilder("0");
+        StringBuilder sb2 = new StringBuilder("1");
+        for (int i = 0; i < steps; i++) {
+            String tmp = sb1.toString();
+            sb1.append(sb2);
+            sb2.append(tmp);
+        }
+        System.out.println(sb1);
+    }
 }
